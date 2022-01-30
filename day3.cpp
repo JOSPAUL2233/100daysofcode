606e602a0f5c6e3ba7ab462fd4ee190e9aca0348
JosPaul <JosPaul223@gmail.com>
2022-01-30 22:47:24 +0530

    day3

---
+++ day3.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+int main(){
+//-----------------------------------------------------------------------------------------------------------
+//bitwise operation
+        // int a = 4;
+        // int b = 6;
+        // cout<<(a&b);
+        //cout<<(a|b);
+//-----------------------------------------------------------------------------------------------------------------------
+        //FIBONACCI SERIES
+        // int n;
+        // cout<<"Enter the value of n : ";
+        // cin>>n;
+        // int first = 0;
+        // int second = 1;
+        // int third = 0;
+        // cout<<first<<" "<<second<<" ";
+        // for(int i = 0;i<n-2;i++){
+
+        //          third = first+second;
+        //          cout<<third<<" ";
+        //          first = second;
+        //          second = third;
+
+
+        // }
+//----------------------------------------------------------------------------------------------------------------
+//checking PRIME NUMBER.
+// int n;
+// cout<<"Enter a number to check whether it is a prime number or not :";
+// cin>>n;
+// bool flag = 1;
+// for(int i = 2;i<n;i++){
+
+// if(n%i == 0){
+//     flag = 0;  
+// }
+
+// }
+
+//     if(flag){
+//     cout<<"It's a prime number."<<endl;
+//     }else{
+//     cout<<"It's not a prime number."<<endl;
+// }
+//----------------------------------------------------------------------------------------------------------------
+//count the number of 1s in a number's binary number.
+int n;
+cout<<"Give any number to check the 1s in its binary number: ";
+cin>>n;
+int count = 0;
+while(n!=0){
+
+    if(n&1){
+       
+        count++;
+    }
+    n=n>>1;
+
+}
+
+ cout<<"Numver of 1s : "<<count<<endl;
+
+
+
+
+
+    return 0;
+}
