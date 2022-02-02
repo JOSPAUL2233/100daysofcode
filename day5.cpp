3ab1135945686ac7a6db5787fae9bce1a04322aa
JosPaul <JosPaul223@gmail.com>
2022-02-02 16:25:05 +0530

    commiting

---
+++ day5.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //-------------------------------------------------------------------------------------
+//Reversing a number
+    // int n=0;
+    // int rev=0;
+    // cout<<"Enter a number : ";
+    // cin>>n;
+    
+    
+
+    // while(n!=0){
+
+    //     rev = (rev)*10 + n%10;
+
+   
+    // n/=10;
+        
+    // }
+
+    // cout<<"Reverse of the given number : "<<rev;
+    
+
+
+    return 0;
+}
